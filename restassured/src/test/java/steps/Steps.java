bf315345fccb6a8da8ed4a2682634c4d79a530a5
Corey Stacy <stacy.corey1992@gmail.com>
2022-06-22 13:32:05 -0400

    a

--- restassured/src/test/java/steps/Steps.java
+++ restassured/src/test/java/steps/Steps.java
@@ -2,6 +2,10 @@ package steps;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 import static io.restassured.RestAssured.*;
 
 import java.util.ArrayList;
@@ -12,18 +16,19 @@ import java.util.Map;
 import org.json.simple.JSONObject;
 
 public class Steps {
-	
+
 	@When("I make a post request")
 	public void newPost() {
 		List<Object> empty = new ArrayList<Object>();
 		baseURI = "https://6143a99bc5b553001717d06a.mockapi.io/testapi/v1//Users";
-		Map<String,Object> map = new HashMap<String,Object>();
+		RequestSpecification req = given();
+		Map<String, Object> map = new HashMap<String, Object>();
 
-		map.put( "createdAt"  , 1631825833);
-		map.put("employee_firstname"  ,  "TestData12345");
-		map.put("employee_lastname"  ,  "TestData12345");
-		map.put("employee_phonenumbe"  ,  "264-783-9453" );
-		map.put("ademployee_emaildress"  ,  "ademployee_emaildress 1");
+		map.put("createdAt", 1631825833);
+		map.put("employee_firstname", "TestData12345");
+		map.put("employee_lastname", "TestData12345");
+		map.put("employee_phonenumbe", "264-783-9453");
+		map.put("ademployee_emaildress", "ademployee_emaildress 1");
 		map.put(" citemployee_addressy", "citemployee_addressy 1");
 		map.put("stateemployee_dev_level", "stateemployee_dev_level 1");
 		map.put("employee_gender", "employee_gender 1");
@@ -32,13 +37,20 @@ public class Steps {
 		map.put("tech_stack", empty);
 		map.put("project", empty);
 		JSONObject json = new JSONObject(map);
-		given().header("Content-Type","application/json").body(map).when().post().then().statusCode(200);
+
+		Response response = given().header("Content-type", "application/json").and().body(json).when().post().then()
+				.extract().response();
+
+		System.out.println(response.getStatusCode());
 	}
-	
+
 	@Then("A new record should be created")
 	public void checkRecord() {
-		baseURI = "https://6143a99bc5b553001717d06a.mockapi.io/testapi/v1/Users";
-		given().get().then().statusCode(200);
+		baseURI = "https://6143a99bc5b553001717d06a.mockapi.io/testapi/v1//Users";
+
+		Response response = given().contentType(ContentType.JSON).when().get().then().extract().response();
+
+		System.out.println(response.getStatusCode());
 	}
 
 }
